0756ff3168de2c7a9d73f1ade88208cf7f92fc67
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2020-09-05 14:20:59 +0530

    :construction: adds recyclerView

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.notes.adapters.NotesRecyclerAdapter;
+import com.example.notes.model.Note;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private RecyclerView mRecyclerView;
+
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        initRecyclerView();
+        //fakeData();
+        
     }
+
+//    private void fakeData(){
+//        for(int i=0;i<10;i++){
+//            Note note = new Note("title","content","timestamp");
+//            mNotes.add(note);
+//        }
+//        mAdapter.notifyDataSetChanged();
+//    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
 }
